4bcd4f39383a2cd105441158a75cd718e55a662c
Windows <Windows@DESKTOP-QCE28GP>
2020-10-04 13:38:50 +0200

    Přidejte soubory projektu.

---
+++ Soucin_cisel_pomoci_souctu/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Soucin_cisel_pomoci_souctu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
